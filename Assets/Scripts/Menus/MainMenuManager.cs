5a5aae378e1df608312262e9b75e8ee146461d4b
Alex Cline <clinealex10@gmail.com>
2022-07-15 21:32:24 -0500

    Main Menu

---
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
