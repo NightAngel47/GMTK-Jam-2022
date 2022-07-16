25a439dd0ea02001d1f1343bbc0d3738b93daf25
Alex Cline <clinealex10@gmail.com>
2022-07-16 08:26:04 -0500

    Main Menu to game scene

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void Options()
